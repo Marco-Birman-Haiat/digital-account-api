f84738ae2f19587086b737c5e3debf5b713c66b9
Marco Haiat <marco.haiat@gmail.com>
2023-08-16 09:30:30 -0300

    feat: error response and account DTO created

---
+++ src/main/java/com/myprojects/digital/account/controllers/dto/ErrorResponseDTO.java
@@ -0,0 +1,4 @@
+package com.myprojects.digital.account.controllers.dto;
+
+public record ErrorResponseDTO(String message) {
+}
---
+++ src/main/java/com/myprojects/digital/account/controllers/dto/accounts/AccountDTO.java
@@ -0,0 +1,21 @@
+package com.myprojects.digital.account.controllers.dto.accounts;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.myprojects.digital.account.models.entities.Account;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
+public record AccountDTO(
+        Long id,
+        @Size(min = 2)
+        @NotNull(message = "name should not be null or blank")
+        String name,
+        @Size(min = 1)
+        @NotNull(message = "document should not be null or blank")
+        String document,
+        @NotNull(message = "available-value should not be null")
+        @JsonProperty("available-value")
+        BigDecimal availableValue) {
+}
