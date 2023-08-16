a07efde15d6c4038c39103797d5ce9e15f6fe1f4
Marco Haiat <marco.haiat@gmail.com>
2023-08-16 09:29:22 -0300

    feat: account entity and repository created

---
+++ src/main/java/com/myprojects/digital/account/models/entities/Account.java
@@ -0,0 +1,91 @@
+package com.myprojects.digital.account.models.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String document;
+
+    @OneToMany(mappedBy = "senderAccount")
+    @JsonIgnore
+    private List<Transfer> transfersSent;
+
+    @OneToMany(mappedBy = "receiverAccount")
+    @JsonIgnore
+    private List<Transfer> transfersReceived;
+
+    @JsonProperty("available-value")
+    @Column(name = "available_value")
+    private BigDecimal availableValue;
+
+    public Account() {
+    }
+
+    public Account(Long id, String name, String document, BigDecimal availableValue) {
+        this.id = id;
+        this.name = name;
+        this.document = document;
+        this.availableValue = availableValue;
+    }
+
+    public List<Transfer> getTransfersSent() {
+        return transfersSent;
+    }
+
+    public void setTransfersSent(List<Transfer> transfersSent) {
+        this.transfersSent = transfersSent;
+    }
+
+    public List<Transfer> getTransfersReceived() {
+        return transfersReceived;
+    }
+
+    public void setTransfersReceived(List<Transfer> transfersReceived) {
+        this.transfersReceived = transfersReceived;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public BigDecimal getAvailableValue() {
+        return availableValue;
+    }
+
+    public void setAvailableValue(BigDecimal availableValue) {
+        this.availableValue = availableValue;
+    }
+}
