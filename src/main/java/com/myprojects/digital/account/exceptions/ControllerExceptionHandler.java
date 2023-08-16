9f75dfbef489ff255809d0348f8f9add91eb89d0
Marco Haiat <marco.haiat@gmail.com>
2023-08-16 09:35:46 -0300

    feat: controller exception handler

---
+++ src/main/java/com/myprojects/digital/account/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,71 @@
+package com.myprojects.digital.account.exceptions;
+
+
+import com.myprojects.digital.account.controllers.dto.ErrorResponseDTO;
+import jakarta.validation.UnexpectedTypeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler({
+            InsufficientValueException.class,
+            DuplicatedTransferException.class,
+            DocumentInUseException.class
+    })
+    public ResponseEntity<ErrorResponseDTO> handleUnprocessableException(
+            RuntimeException exception) {
+
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body(errorResponseDTO);
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleNotFoundException(
+            AccountNotFoundException exception) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(errorResponseDTO);
+    }
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            UnexpectedTypeException.class
+    })
+    public ResponseEntity<ErrorResponseDTO> handleInvalidRequestException(
+            Exception exception) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(exception.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponseDTO);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleThrowable(Throwable exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_GATEWAY)
+                .body(exception.getMessage());
+    }
+}
